22187e684211aa8ac58900a154330313c3a923ee
Yelyzaveta Tymoshenko <tymoshenkoyelyzaveta.s@gmail.com>
2026-05-17 14:19:37 +0200

    Fix styles of the Form

--- src/AddBook.tsx
+++ src/AddBook.tsx
@@ -32,18 +32,25 @@ const AddBook = ({ selectedCountry, onCancel }: AddBookProps) => {
   const { code, name } = selectedCountry;
   return (
     <form action={formAction} className="form">
+      <label className="label" htmlFor="country">
+        Country
+      </label>
       <TextInput id="country" type="text" disabled={true} value={name} />
+      <label className="label" htmlFor="title">
+        Title *
+      </label>
       <TextInput id="title" type="text" value="" />
+      <label className="label" htmlFor="author">
+        Author *
+      </label>
       <TextInput id="author" type="text" value="" />
       <TextInput type="hidden" name="countryCode" value={code} />
-
       <div className="actions">
         <button type="button" className="button" onClick={onCancel}>
           Cancel
         </button>
         <SubmitButton />
       </div>
-
       {state?.error && <p>{state.error}</p>}
       {/* Todo: add toast */}
       {state?.success && <p>Saved!</p>}
